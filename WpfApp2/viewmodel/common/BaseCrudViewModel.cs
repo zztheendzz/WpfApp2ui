2ab69d614e2b023a66a41c8866151239b9e131bc
auto-trong <nguyentrong7396@gmail.com>
2026-03-15 16:58:48 +0700

    ui search

---
+++ WpfApp2/viewmodel/common/BaseCrudViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using WpfApp2.command;
+
+namespace WpfApp2.viewmodel.common
+{
+    public class BaseCrudViewModel<T> : INotifyPropertyChanged where T : class
+    {
+        protected BaseService<T> _service;
+
+        public ObservableCollection<T> Items { get; set; }
+
+        public T SelectedItem { get; set; }
+
+        public ICommand AddCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+        public ICommand UpdateCommand { get; set; }
+
+        public BaseCrudViewModel(BaseService<T> service)
+        {
+            _service = service;
+
+            Items = new ObservableCollection<T>(_service.GetAll());
+
+            AddCommand = new RelayCommand(Add);
+            DeleteCommand = new RelayCommand(Delete);
+            UpdateCommand = new RelayCommand(Update);
+        }
+
+        void Add(object obj)
+        {
+            if (SelectedItem == null) return;
+
+            _service.Add(SelectedItem);
+            Items.Add(SelectedItem);
+        }
+
+        void Delete(object obj)
+        {
+            if (SelectedItem == null) return;
+
+            _service.Delete(SelectedItem);
+            Items.Remove(SelectedItem);
+        }
+
+        void Update(object obj)
+        {
+            if (SelectedItem == null) return;
+
+            _service.Update(SelectedItem);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ WpfApp2/viewmodel/common/BaseService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WpfApp2.Services;
+using System.Collections.Generic;
+using Dapper.Contrib.Extensions;
+
+namespace WpfApp2.viewmodel.common
+{
+        public class BaseService<T> where T : class
+        {
+            protected DatabaseService _db = new DatabaseService();
+
+            public IEnumerable<T> GetAll()
+            {
+                using var conn = _db.GetConnection();
+                return conn.GetAll<T>();
+            }
+
+            public long Add(T model)
+            {
+                using var conn = _db.GetConnection();
+                return conn.Insert(model);
+            }
+
+            public bool Update(T model)
+            {
+                using var conn = _db.GetConnection();
+                return conn.Update(model);
+            }
+
+            public bool Delete(T model)
+            {
+                using var conn = _db.GetConnection();
+                return conn.Delete(model);
+            }
+        }
+}
